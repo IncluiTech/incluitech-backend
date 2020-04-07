3d11440178e0c77b7076080889ab2139f75414ab
CarlosAsrc <carlos.asrc@gmail.com>
2020-04-07 11:59:47 -0300

    Refatoração da configuração do database de desenvolvimento.

--- database/init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "incluitech" <<-EOSQL
-    GRANT ALL PRIVILEGES ON DATABASE "incluitech" TO "postgres";
-    CREATE TABLE IF NOT EXISTS usuario(
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR (50) NOT NULL,
-    data_criacao TIMESTAMP NOT NULL DEFAULT NOW());
-EOSQL
