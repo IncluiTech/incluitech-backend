bea2f0e63b325e59a14f3ab9c0c4ac47af83b31e
alexiadorneles <lecavsalexia@gmail.com>
2020-04-14 23:45:44 -0300

    test(client): add tests to client service.

--- solucao-de-problemas-service/src/test/java/com/ages/incuitech/backend/solucaodeproblemasservice/usuario/UsuarioStub.java
+++ solucao-de-problemas-service/src/test/java/com/ages/incuitech/backend/solucaodeproblemasservice/usuario/UsuarioStub.java
@@ -1,5 +1,6 @@
 package com.ages.incuitech.backend.solucaodeproblemasservice.usuario;
 
+import com.ages.incuitech.backend.solucaodeproblemasservice.api.domain.StatusCadastro;
 import com.ages.incuitech.backend.solucaodeproblemasservice.api.usuario.UsuarioRequest;
 import com.ages.incuitech.backend.solucaodeproblemasservice.business.usuario.Usuario;
 
@@ -9,9 +10,13 @@ public class UsuarioStub {
     }
 
     public static Usuario usuario() {
-        Usuario usuario = new Usuario();
-        usuario.setId(1l);
-        usuario.setNome("fulano");
-        return usuario;
+        return Usuario.builder()
+                .id(1L)
+                .email("fulano@gmail.com")
+                .nome("Fulano")
+                .sobrenome("de Tal")
+                .statusCadastro(StatusCadastro.P)
+                .especialidades(null)
+                .build();
     }
 }
