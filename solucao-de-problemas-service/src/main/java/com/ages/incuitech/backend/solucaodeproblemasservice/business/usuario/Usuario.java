eeaf076a05342ccfec61d5b99dc9eb643af870b0
alexiadorneles <lecavsalexia@gmail.com>
2020-04-14 23:29:00 -0300

    feat(usuario): small changes in mapper and entity.

--- solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/usuario/Usuario.java
+++ solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/usuario/Usuario.java
@@ -1,12 +1,23 @@
 package com.ages.incuitech.backend.solucaodeproblemasservice.business.usuario;
 
+import com.ages.incuitech.backend.solucaodeproblemasservice.api.domain.Especialidade;
+import com.ages.incuitech.backend.solucaodeproblemasservice.api.domain.StatusCadastro;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 
+import java.util.List;
+
+@Getter
 public class Usuario {
 
     @Id
     private Long id;
     private String nome;
+    private String sobrenome;
+    private String email;
+    private List<Especialidade> especialidades;
+    private StatusCadastro statusCadastro;
+
 
     public String getNome() {
         return nome;
--- solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/usuario/UsuarioMapper.java
+++ solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/usuario/UsuarioMapper.java
@@ -2,8 +2,12 @@ package com.ages.incuitech.backend.solucaodeproblemasservice.business.usuario;
 
 import com.ages.incuitech.backend.solucaodeproblemasservice.api.usuario.UsuarioRequest;
 import com.ages.incuitech.backend.solucaodeproblemasservice.api.usuario.UsuarioResponse;
+import com.ages.incuitech.backend.solucaodeproblemasservice.api.usuario.cliente.ClienteResponse;
 
 public class UsuarioMapper {
+
+    private UsuarioMapper() {
+    }
 
     public static Usuario mapToModel(UsuarioRequest usuarioRequest) {
         Usuario usuario = new Usuario();
@@ -13,5 +17,15 @@ public class UsuarioMapper {
 
     public static UsuarioResponse mapToResponse(Usuario usuario) {
         return new UsuarioResponse(usuario.getId(), usuario.getNome());
+    }
+
+    public static ClienteResponse mapToClienteResponse(Usuario usuario) {
+        return ClienteResponse.builder()
+                .nome(usuario.getNome())
+                .id(usuario.getId())
+                .email(usuario.getEmail())
+                .especialidades(usuario.getEspecialidades())
+                .statusCadastro(usuario.getStatusCadastro())
+                .build();
     }
 }
