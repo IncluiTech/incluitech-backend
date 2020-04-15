4fa763a87859e54cc37ce6af907bd5042995ab59
alexiadorneles <lecavsalexia@gmail.com>
2020-04-14 23:23:48 -0300

    feat(sql): update init sql to have more columns.

--- database/init.sql
+++ database/init.sql
@@ -3,5 +3,9 @@ GRANT ALL PRIVILEGES ON DATABASE "incluitech" TO "postgres";
 CREATE TABLE IF NOT EXISTS usuario(
     id SERIAL PRIMARY KEY,
     nome VARCHAR (50) NOT NULL,
+    sobrenome VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    status_cadastro VARCHAR(1) NOT NULL,
+    especialidades VARCHAR(10),
     data_criacao TIMESTAMP NOT NULL DEFAULT NOW()
 );
