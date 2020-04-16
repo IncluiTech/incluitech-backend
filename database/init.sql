c4615c3fcf15d23fb2dc6af4cf2c10e878cd7db1
alexiadorneles <lecavsalexia@gmail.com>
2020-04-15 21:45:49 -0300

    feat(database): add client table.

--- database/init.sql
+++ database/init.sql
@@ -1,11 +1,17 @@
 GRANT ALL PRIVILEGES ON DATABASE "incluitech" TO "postgres";
 
-CREATE TABLE IF NOT EXISTS usuario(
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR (50) NOT NULL,
-    sobrenome VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    status_cadastro VARCHAR(1) NOT NULL,
-    especialidades VARCHAR(10),
-    data_criacao TIMESTAMP NOT NULL DEFAULT NOW()
+create table if not exists usuario(
+    id serial primary key,
+    nome varchar (50) not null,
+    data_criacao timestamp not null default now()
+);
+
+create table if not exists cliente(
+    id serial primary key,
+    nome varchar (50) not null,
+    sobrenome varchar(50) not null,
+    email varchar(50) not null,
+    status_cadastro varchar(1) not null,
+    especialidades varchar(10),
+    data_criacao timestamp not null default now()
 );
