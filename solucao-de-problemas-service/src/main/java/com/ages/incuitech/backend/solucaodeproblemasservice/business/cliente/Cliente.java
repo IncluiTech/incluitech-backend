680c2f8bf93a299d26f5e96b45e0bddf0abfcd9e
alexiadorneles <lecavsalexia@gmail.com>
2020-04-15 22:03:14 -0300

    refactor(cliente): remove unnecessary methods from model.

--- solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/cliente/Cliente.java
+++ solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/cliente/Cliente.java
@@ -20,20 +20,4 @@ public class Cliente {
     private List<Especialidade> especialidades;
     private StatusCadastro statusCadastro;
 
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
