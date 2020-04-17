0e0d33b224b6433d5422196435aa8cad14057dd9
alexiadorneles <lecavsalexia@gmail.com>
2020-04-16 22:08:57 -0300

    feat(bot): add models from jbot.

---
+++ chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/api/bot/model/QuickReply.java
@@ -0,0 +1,8 @@
+package com.ages.incuitech.backend.chatbotservice.api.bot.model;
+
+public class QuickReply {
+    private String content_type;
+    private String title;
+    private String payload;
+    private String image_url;
+}
