c6b147a218b6d06d8bc5198827ee372bb498a36c
CarlosAsrc <carlos.asrc@gmail.com>
2020-04-16 22:29:47 -0300

    Adds lombok configuration to ClientRequest class.

--- solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/api/cliente/ClienteRequest.java
+++ solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/api/cliente/ClienteRequest.java
@@ -1,48 +1,18 @@
 package com.ages.incuitech.backend.solucaodeproblemasservice.api.cliente;
 
-public class ClienteRequest {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteRequest {
     private String nome;
     private String senha;
     private String sobrenome;
     private String especialidades;
     private String email;
-    public ClienteRequest(){
-    }
-
-    public ClienteRequest(String email, String senha, String nome, String sobrenome, String especialidades) {
-        this.nome = nome;
-        this.senha = senha;
-        this.sobrenome = sobrenome;
-        this.email = email;
-        this.especialidades = especialidades;
-
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-
-
-
-
-    public String getEspecialidades() {
-        return especialidades;
-    }
-
-
-
-
-
-    public String getEmail() {
-        return email;
-    }
-
-
 }
