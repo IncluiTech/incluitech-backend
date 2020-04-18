7a9b88c7fda816032c7febb44512104cb874423f
Vitor Hugo <vitor_hgbds@hotmail.com>
2020-04-18 01:34:26 -0300

    Mapper BotMessage --> InternalMessage

---
+++ chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/api/bot/model/internal/message/MessageMapper.java
@@ -0,0 +1,24 @@
+package com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.message;
+
+import com.ages.incuitech.backend.chatbotservice.api.bot.model.incoming.User;
+import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.bot.message.BotMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+
+public class MessageMapper {
+    
+        private MessageMapper(){
+
+        }
+
+        @GetMapping
+        public static InternalMessage mapToModel(BotMessage message, User user){
+                return InternalMessage.builder()
+                        .user(user)
+                        .map(message.getContexto())
+                        .messageType(message.getType())
+                        .payload()
+                        .build();
+        }
+    
+}
