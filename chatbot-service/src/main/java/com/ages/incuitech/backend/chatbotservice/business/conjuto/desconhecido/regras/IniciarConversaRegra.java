7f1564e5b707cd96f9e08779241f28cfe0c478c2
Julia Makowski <julia.makowski@edu.pucrs.br>
2020-04-24 00:00:11 -0300

    Criaçao classes de regra de contato

--- chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/business/conjuto/desconhecido/regras/IniciarConversaRegra.java
+++ chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/business/conjuto/desconhecido/regras/IniciarConversaRegra.java
@@ -8,29 +8,30 @@ import com.ages.incuitech.backend.chatbotservice.business.conjuto.RegraDoBot;
 import java.util.*;
 
 public class IniciarConversaRegra implements RegraDoBot {
-
     @Override
     public boolean verifica(MensagemInterna message) {
         return true;
+
     }
 
     @Override
     public BotMessage processa(MensagemInterna message) {
         Map<String, Object> contexto = new HashMap<>();
         contexto.put("jaFezSaudacao", true);
-
-
         return new BotMessage(contexto).withMessages(
                 Arrays.asList(
                         new TextComponentBotMessage("Olá, Eu Sou a Helena! Consultora e especialista da Incluitec."),
                         new TextComponentBotMessage("Vi que é sua primeira vez por aqui, então antes de começarmos, " +
                                 "preciso de algumas informações suas para entrar em contato."),
+                        new TextComponentBotMessage("Para isso preciso que responda algumas perguntas: "),
                         new QuickReplyComponentBotMessage("Como prefere que eu entre em contato com você?",
-                                Arrays.asList(new QuickReplyButton("Email","escolhaContato"),
+                                Arrays.asList(
+                                        new QuickReplyButton("Email","escolhaContato"),
                                         new QuickReplyButton("Telefone/Whatsapp", "escolhaContato"),
-                                        new QuickReplyButton("Email e Telefone", "escolhaContato"))))
+                                        new QuickReplyButton("Email e Telefone", "escolhaContato")
+                                )
+                        )
+                )
         );
-
-
     }
 }
