684a3d5f4a477d93d44b2f8eaf9cb14ab2fbe784
CarlosAsrc <carlos.asrc@gmail.com>
2020-04-24 09:29:40 -0300

    Configure multi-stage builds on dockerfiles, commit gradle folder, and adapts start.sh.

--- start.sh
+++ start.sh
@@ -2,15 +2,5 @@
 docker rm solucao-de-problemas-service chatbot-service incluitech-postgres
 # REMOVE IMAGENS DA APLICAÇÃO
 docker rmi incluitech-backend_solucao-de-problemas-service incluitech-backend_incluitech-postgres incluitech-backend_chatbot-service
-
-
-cd chatbot-service/
-gradle wrapper
-./gradlew build
-
-cd ../solucao-de-problemas-service/
-gradle wrapper
-./gradlew build
-
-cd ..
+# RODA O DOCKER COMPOSE COM OS DOIS SERVIÇOS SPRINGBOOT E O BANCO DE DADOS.
 docker-compose up
