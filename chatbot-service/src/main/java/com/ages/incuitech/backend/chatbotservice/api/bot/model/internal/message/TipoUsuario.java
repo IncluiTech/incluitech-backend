ce19f5428408e70d8e2fedc69bb4d9d78dec43bc
alexiadorneles <lecavsalexia@gmail.com>
2020-04-30 22:05:21 -0300

    feat(bot): adicionar funcionalidade em TipoUsuario.

--- chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/api/bot/model/internal/message/TipoUsuario.java
+++ chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/api/bot/model/internal/message/TipoUsuario.java
@@ -3,10 +3,19 @@ package com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.message
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 @AllArgsConstructor
 public enum TipoUsuario {
     SOLUCIONADOR("solucionador"), CLIENTE("cliente");
 
     private String tipo;
+
+    public static TipoUsuario getFromTipo(String tipo) {
+        return Arrays.stream(TipoUsuario.values())
+                .filter(contato -> contato.tipo.equals(tipo))
+                .findAny()
+                .orElse(null);
+    }
 }
