4aab5f47d0f0d5507933fc9102801f75e43994a6
CarlosAsrc <carlos.asrc@gmail.com>
2020-05-18 08:31:11 -0300

    Remove classes e métodos desnecessários

--- solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/tag/TagService.java
+++ solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/tag/TagService.java
@@ -1,18 +1,14 @@
 package com.ages.incuitech.backend.solucaodeproblemasservice.business.tag;
 
-import com.ages.incuitech.backend.solucaodeproblemasservice.api.tag.TagResponse;
 import com.ages.incuitech.backend.solucaodeproblemasservice.business.GenericCRUDService;
 import com.ages.incuitech.backend.solucaodeproblemasservice.infrastructure.tags.TagRepository;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,13 +17,6 @@ public class TagService extends GenericCRUDService<Tag, Long, TagRepository> {
     @Inject
     public void setRepository(TagRepository repository) {
         this.repository = repository;
-    }
-
-    public List<TagResponse> findAllTags() {
-        return this.findAll()
-                .stream()
-                .map(TagMapper::mapToResponse)
-                .collect(Collectors.toList());
     }
 
     public Tag salvar(String tag) {
