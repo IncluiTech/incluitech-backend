bc95acc28ac93dd009cf6c5f54c48d80af49caaf
Vitor Hugo <vitor_hgbds@hotmail.com>
2020-05-28 21:19:30 -0300

    criando novas regras do cliente

--- chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/business/conjunto/cliente/regras/ConfirmarTagsRegra.java
+++ chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/business/conjunto/cliente/regras/ConfirmarTagsRegra.java
@@ -1,4 +1,35 @@
 package com.ages.incuitech.backend.chatbotservice.business.conjunto.cliente.regras;
 
-public class ConfirmarTagsRegra {
+import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.bot.message.BotMessage;
+import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.bot.message.TextComponentBotMessage;
+import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.message.MensagemInterna;
+import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.message.TipoUsuario;
+import com.ages.incuitech.backend.chatbotservice.business.conjunto.RegraDoBot;
+import com.ages.incuitech.backend.chatbotservice.business.provider.BotMessageProvider;
+
+import java.util.ArrayList;
+
+public class ConfirmarTagsRegra implements RegraDoBot {
+
+    private final BotMessageProvider<TipoUsuario> provider;
+
+    public ConfirmarTagsRegra(BotMessageProvider<TipoUsuario> provider) {
+        this.provider = provider;
+    }
+
+    @Override
+    public boolean verifica(MensagemInterna message) {
+        return message.getContexto().propertyIsEqualsTo("aguardandoConfirmacaoTags", true);
+    }
+
+    @Override
+    public BotMessage processa(MensagemInterna message) {
+        message.getContexto().remove("aguardandoConfirmacaoTags");
+        message.getContexto().put("primeiraMensagemUsuarioComTipo", true);
+        message.getContexto().put("instituicoes", new ArrayList<String>());
+        message.getContexto().put("areasAtuacao", new ArrayList<String>());
+        BotMessage botMessage = provider.provide(TipoUsuario.CLIENTE, message.getContexto());
+        botMessage.getMessages().add(0, new TextComponentBotMessage("Então, por favor, repita a seleção de tags."));
+        return botMessage;
+    }
 }
