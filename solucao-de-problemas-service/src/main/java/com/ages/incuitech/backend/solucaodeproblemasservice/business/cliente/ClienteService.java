6d308470ef1254b1ef65190ec1e9279f13082b73
jpmartins <joao@ubots.com.br>
2020-06-04 22:04:16 -0300

    Adicionando teste de update e ajuste posicionamento da classe

--- solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/cliente/ClienteService.java
+++ solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/business/cliente/ClienteService.java
@@ -3,10 +3,8 @@ package com.ages.incuitech.backend.solucaodeproblemasservice.business.cliente;
 import com.ages.incuitech.backend.solucaodeproblemasservice.ChatBotClient;
 import com.ages.incuitech.backend.solucaodeproblemasservice.api.cliente.ClienteRequest;
 import com.ages.incuitech.backend.solucaodeproblemasservice.api.cliente.ClienteResponse;
-import com.ages.incuitech.backend.solucaodeproblemasservice.api.solucionador.SolucionadorRequest;
 import com.ages.incuitech.backend.solucaodeproblemasservice.business.GenericCRUDService;
 import com.ages.incuitech.backend.solucaodeproblemasservice.business.cliente.exception.ClienteNaoEncontradoException;
-import com.ages.incuitech.backend.solucaodeproblemasservice.business.solucionador.Solucionador;
 import com.ages.incuitech.backend.solucaodeproblemasservice.business.tag.Tag;
 import com.ages.incuitech.backend.solucaodeproblemasservice.business.tag.TagMapper;
 import com.ages.incuitech.backend.solucaodeproblemasservice.business.tag.TagService;
@@ -16,7 +14,6 @@ import com.ages.incuitech.backend.solucaodeproblemasservice.infrastructure.tags.
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -60,28 +57,6 @@ public class ClienteService extends GenericCRUDService<Cliente, Long, ClienteRep
         }
     }
 
-    private Map<Long, List<String>> buscarTodasAsTagsDosClientes() {
-        return tagClienteRepository.findAllLinkedTags().stream()
-                .collect(groupingBy(UserTag::getUserId,
-                        mapping(UserTag::getTagName, toList())));
-    }
-
-    private List<Tag> salvarTags(List<String> tags) {
-        return tags.stream()
-                .map(tagService::salvar)
-                .collect(toList());
-    }
-
-    private void salvarTagsCliente(Long clienteId, List<Tag> tags) {
-        tagClienteRepository.saveAll(TagMapper.buildTagCliente(tags, clienteId));
-    }
-
-    private List<Tag> persistirTagsCliente(ClienteRequest clienteRequest, Cliente clienteSalvo) {
-        List<Tag> tags = salvarTags(clienteRequest.getTags());
-        salvarTagsCliente(clienteSalvo.getId(), tags);
-        return tags;
-    }
-
     public ClienteResponse update(ClienteRequest request) {
         Cliente entity = this.repository.findByIdFacebook(request.getFacebookId());
         request.setId(entity.getId());
@@ -104,4 +79,26 @@ public class ClienteService extends GenericCRUDService<Cliente, Long, ClienteRep
         throw new ClienteNaoEncontradoException();
     }
 
+
+    private Map<Long, List<String>> buscarTodasAsTagsDosClientes() {
+        return tagClienteRepository.findAllLinkedTags().stream()
+                .collect(groupingBy(UserTag::getUserId,
+                        mapping(UserTag::getTagName, toList())));
+    }
+
+    private void salvarTagsCliente(Long clienteId, List<Tag> tags) {
+        tagClienteRepository.saveAll(TagMapper.buildTagCliente(tags, clienteId));
+    }
+
+    private List<Tag> persistirTagsCliente(ClienteRequest clienteRequest, Cliente clienteSalvo) {
+        List<Tag> tags = salvarTags(clienteRequest.getTags());
+        salvarTagsCliente(clienteSalvo.getId(), tags);
+        return tags;
+    }
+
+    private List<Tag> salvarTags(List<String> tags) {
+        return tags.stream()
+                .map(tagService::salvar)
+                .collect(toList());
+    }
 }
