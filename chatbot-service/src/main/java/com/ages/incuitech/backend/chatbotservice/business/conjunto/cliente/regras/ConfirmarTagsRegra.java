2f3baf731170012a55cc22d34188ed4a82723cfc
CarlosAsrc <carlos.asrc@gmail.com>
2020-06-12 02:11:21 -0300

    Ajusta salvamento de tags, troca porta do banco no docker compose, e modifica mensagens de area de atuaçao do cliente

--- chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/business/conjunto/cliente/regras/ConfirmarTagsRegra.java
+++ chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/business/conjunto/cliente/regras/ConfirmarTagsRegra.java
@@ -1,6 +1,5 @@
 package com.ages.incuitech.backend.chatbotservice.business.conjunto.cliente.regras;
 
-import com.ages.incuitech.backend.chatbotservice.api.bot.model.Contexto;
 import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.bot.message.BotMessage;
 import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.bot.message.QuickReplyComponentBotMessage;
 import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.bot.message.TextComponentBotMessage;
@@ -8,7 +7,6 @@ import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.message.
 import com.ages.incuitech.backend.chatbotservice.api.bot.model.internal.message.TipoUsuario;
 import com.ages.incuitech.backend.chatbotservice.api.bot.model.outgoing.button.QuickReplyButton;
 import com.ages.incuitech.backend.chatbotservice.business.conjunto.RegraDoBot;
-import com.ages.incuitech.backend.chatbotservice.business.domain.ProblemasCliente;
 import com.ages.incuitech.backend.chatbotservice.business.domain.SimNao;
 import com.ages.incuitech.backend.chatbotservice.business.provider.BotMessageProvider;
 import com.ages.incuitech.backend.chatbotservice.infrastructure.SolucaoDeProblemasClient;
--- chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/business/conjunto/cliente/regras/PerguntasTagsRegra.java
+++ chatbot-service/src/main/java/com/ages/incuitech/backend/chatbotservice/business/conjunto/cliente/regras/PerguntasTagsRegra.java
@@ -20,7 +20,7 @@ import static java.util.stream.Collectors.toList;
 public class PerguntasTagsRegra implements RegraDoBot {
 
     private static final String FINALIZAR_TAGS = "Finalizar Tags";
-    private static final List<String> TAGS_INICIAIS = Arrays.asList("ONG", "Escola", "Empresa", "Pessoa Física");
+    private static final List<String> TAGS_INICIAIS = Arrays.asList("Oficinas e Cursos", "Talentos", "Informação e apoio", "Consultoria", "Suporte Emocional");
 
     @Override
     public boolean verifica(MensagemInterna message) {
