f9b2113c146cde6ffc3f74148a068bd9d9d87ba7
CarlosAsrc <carlos.asrc@gmail.com>
2020-06-18 22:41:50 -0300

    Implementa notificação de email para adm quando um problema for cadastrado

--- solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/ChatBotClient.java
+++ solucao-de-problemas-service/src/main/java/com/ages/incuitech/backend/solucaodeproblemasservice/ChatBotClient.java
@@ -1,15 +1,14 @@
 package com.ages.incuitech.backend.solucaodeproblemasservice;
 
-import com.ages.incuitech.backend.solucaodeproblemasservice.api.cliente.*;
-import com.ages.incuitech.backend.solucaodeproblemasservice.api.solucionador.*;
-import com.google.gson.*;
-import lombok.*;
-import lombok.extern.slf4j.*;
-import org.springframework.http.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.client.*;
+import com.ages.incuitech.backend.solucaodeproblemasservice.api.solucionador.SolucionadorRequest;
+import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
-import static java.lang.String.*;
+import static java.lang.String.format;
 
 @Slf4j
 @Component
